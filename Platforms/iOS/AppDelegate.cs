ecd61e1ac2e0c57c8917dd07ee2c8d97e102abd6
Christian Franco <christianfrancosoto@gmail.com>
2025-05-05 20:17:58 +0200

    Project creation

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace swissbyte
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
